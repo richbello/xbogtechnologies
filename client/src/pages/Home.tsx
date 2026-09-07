7c4ddfb6f81cf5189ee0ba6401f1bdf0318fd761
Richard Bello Roncancio <111403635+richbello@users.noreply.github.com>
2026-09-07 21:18:00 +0000

    revert: volver a ModernPortfolio

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import PortfolioModerno from "@/components/FusionPortfolio";
+import PortfolioModerno from "@/components/ModernPortfolio";
 
 export default function Home() {
   return <PortfolioModerno />;
